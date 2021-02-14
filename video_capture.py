3a72220307868b0800ebb3639b9576db740203aa
Aman <aman.s.chhina@gmail.com>
2021-02-14 04:55:27 -0500

    Added frame to main display and removed plots from prediction() function.

--- video_capture.py
+++ video_capture.py
@@ -11,10 +11,8 @@ import numpy as np
 # To capture frames from web camera and detect faces from frames
 import cv2
 
-import matplotlib.pyplot as plt
-
 # Stops TF optimization warnings from displaying
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Path to dataset, to model, and to save/load history of model
 MODEL_PATH = 'asl_model'
@@ -28,6 +26,7 @@ model = load_model(MODEL_PATH)
 DATASET_PATH = './dataset'
 TRAIN_PATH = DATASET_PATH + '/asl_train'
 
+GREEN = (0, 255, 0)
 
 # Grabs video capture of default web camera
 vs = cv2.VideoCapture(0)
@@ -38,6 +37,9 @@ time.sleep(5.0)
 # Gets all symbol categories from evaluation path
 symbols = sorted(os.listdir(TRAIN_PATH))
 
+# Dimensions for our cropped frame
+x, y, w, h = 100, 100, 400, 400
+
 
 def prediction(image):
     # Convert to a numpy array, rescales to what we trained our model on and adds additional level of nesting
@@ -46,24 +48,17 @@ def prediction(image):
     image_batch = np.expand_dims(image_array, axis=0)
 
     # Gets prediction of passed image
-    prediction = model.predict(image_batch)
+    predict = model.predict(image_batch)
 
     # Iterates over array and checks to see which category the model predicted
     # and then rewrites prediction to the corresponding symbol
-    print(prediction[0])
     res = 'n/a'
-    for i in range(len(prediction[0])):
-
+    for i in range(len(predict[0])):
         score = 0
-        if prediction[0][i] >= 0.25 and prediction[0][i] > score:
-            score = prediction[0][i]
+
+        if predict[0][i] >= 0.25 and predict[0][i] > score:
+            score = predict[0][i]
             res = symbols[i]
-
-    plt.imshow(image)
-    plt.show()
-
-    # If we want to show our data results, then we will print
-    # the category as well as show the input image that was used.
 
     return res
 
@@ -72,19 +67,14 @@ while True:
     # Capture the current frame and disregard the return value
     _, frame = vs.read()
 
-    x = 100
-    w = 400
-    y = 100
-    h = 400
-
     # Converting frame from BGR to RGB
     crop_img = frame[y:y + h, x:x + w]
     rgb_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
     resized_img = cv2.resize(rgb_img, TARGET_SIZE)
 
     # Display the resulting frame with the provided title
+    cv2.rectangle(frame, (x, y), (x + w, y + h), GREEN, cv2.LINE_4)
     cv2.imshow("Hand Speak", frame)
-    cv2.imshow("cropped", crop_img)
 
     # Wait 1ms between frame captures
     key = cv2.waitKey(100)
